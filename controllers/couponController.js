c94ee925d50e89a38f275da0ac92e759c91a828b
Sharath Mg <sharathraghu@gmail.com>
2017-06-20 15:51:35 -0700

    session and mycoupon html changes

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -1,4 +1,6 @@
 var expressConfig = require('../controllers/systemController');
+const User = require('../classes/user');
+
 var express = expressConfig();
 var couponRouter = express.Router();
 
@@ -8,14 +10,16 @@ couponRouter.post('/newCoupon', function(req, res){
   let coupon = new Coupon();
   var coupons = [];
   var couponSession = req.session;
-  console.log(couponSession);
   coupon.setCouponName(req.body.couponName);
   coupon.setCompanyName(req.body.companyName);
   coupon.setFilePath(req.files.exampleInputFile.name);
   coupon.setFileBinData("data:"+req.files.exampleInputFile.mimetype+";base64,"+new Buffer(req.files.exampleInputFile.data).toString('base64')); 
   coupons.push(coupon);
   coupons.push(coupon);
+ var user = couponSession.user;
+ console.log(user);
   res.render('myCoupons', {
+    user: user,
     coupons: coupons
   });
 });
