e77b67af96d5764cc46109470fe7c758b04f15e9
Sharath Mg <sharathraghu@gmail.com>
2017-07-12 16:09:17 -0700

    pagination changes

--- app/controllers/couponController.js
+++ app/controllers/couponController.js
@@ -13,6 +13,7 @@ couponRouter.post('/newCoupon', function(req, res,next){
   var couponSession = req.session;
   var suser = couponSession.user;
   let user = new User();
+  var perPageLimit = 5;
 
   user.setFisrstName(suser._firstName);
   user.setLastName(suser._lastName);
@@ -31,7 +32,7 @@ couponRouter.post('/newCoupon', function(req, res,next){
     if (err) throw err;
     CouponModel.find({}, function(err, couponsFromDB) {
       if (err) throw err;
-      rtrieveAndDisplay(couponsFromDB,coupons,res, user);
+      rtrieveAndDisplay(couponsFromDB,coupons,res, user, perPageLimit);
       next();
     });
   });
@@ -42,15 +43,17 @@ couponRouter.get('/myCoupons', function(req, res,next){
   var couponSession = req.session;
   var suser = couponSession.user;
   let user = new User();
+  var perPageLimit = 5;
+  var pageNum = req.query.page === undefined?0:req.query.page;
 
   user.setFisrstName(suser._firstName);
   user.setLastName(suser._lastName);
   user.setEmail(suser._email);
   user.setPassword(suser._password);
 
-  CouponModel.find({}, function(err, couponsFromDB) {
+  CouponModel.find({}, {}, {limit: perPageLimit, skip:(perPageLimit * pageNum)}, function(err, couponsFromDB) {
     if (err) throw err;
-    rtrieveAndDisplay(couponsFromDB,coupons,res, user);
+    rtrieveAndDisplay(couponsFromDB,coupons,res, user, perPageLimit);
     next();
   });
 });
@@ -70,7 +73,7 @@ couponRouter.get('/dashboard', function(req, res, next) {
     });
 });
 
-function rtrieveAndDisplay(couponsFromDB, coupons, res, user) {
+function rtrieveAndDisplay(couponsFromDB, coupons, res, user, perPageLimit) {
   couponsFromDB.forEach(function(element) {
         let coupon = new Coupon();
 
@@ -79,11 +82,13 @@ function rtrieveAndDisplay(couponsFromDB, coupons, res, user) {
         coupon.setFileBinData(element.binary_data);  
             
         coupons.push(coupon);          
-  });
+  });  
   res.render('myCoupons', {
     user: user,
-    coupons: coupons
+    coupons: coupons,
+    pagesToDisp:(coupons.length)/perPageLimit
   });
+  log.info((coupons.length)/perPageLimit);
 }
 
 module.exports = couponRouter;
