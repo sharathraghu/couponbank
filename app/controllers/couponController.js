8b342cb493fdc2238271d54144ceba97da07f89f
Sharath Mg <sharathraghu@gmail.com>
2017-07-12 16:26:12 -0700

    pagination changes 2

--- app/controllers/couponController.js
+++ app/controllers/couponController.js
@@ -8,11 +8,12 @@ var log = systemConfig.loggerModule();
 var express = systemConfig.expressModule();
 var couponRouter = express.Router();
 
-couponRouter.post('/newCoupon', function(req, res,next){
+couponRouter.post('/newCoupon', function (req, res, next) {
   var coupons = [];
   var couponSession = req.session;
   var suser = couponSession.user;
   let user = new User();
+  var pagesToDisp = 0;
   var perPageLimit = 5;
 
   user.setFisrstName(suser._firstName);
@@ -22,43 +23,50 @@ couponRouter.post('/newCoupon', function(req, res,next){
 
   var newCoupon = new CouponModel(
     {
-      coupon_name : req.body.couponName,
-      coupon_categoty : req.body.couponCategory,
-      binary_data : req.body.exampleInputFile
+      coupon_name: req.body.couponName,
+      coupon_categoty: req.body.couponCategory,
+      binary_data: req.body.exampleInputFile
     }
   );
 
-  newCoupon.save(function(err) {
+  newCoupon.save(function (err) {
     if (err) throw err;
-    CouponModel.find({}, function(err, couponsFromDB) {
+    CouponModel.find({}, function (err, couponsFromDB) {
       if (err) throw err;
-      rtrieveAndDisplay(couponsFromDB,coupons,res, user, perPageLimit);
+      CouponModel.count({}, function (err, count) {
+      pagesToDisp = count / perPageLimit;
+      rtrieveAndDisplay(couponsFromDB, coupons, res, user, pagesToDisp);
       next();
+    });  
     });
   });
 });
 
-couponRouter.get('/myCoupons', function(req, res,next){
+couponRouter.get('/myCoupons', function (req, res, next) {
   var coupons = [];
   var couponSession = req.session;
   var suser = couponSession.user;
   let user = new User();
   var perPageLimit = 5;
-  var pageNum = req.query.page === undefined?0:req.query.page;
+  var pagesToDisp = 0;
+  var pageNum = req.query.page === undefined ? 0 : req.query.page;
 
   user.setFisrstName(suser._firstName);
   user.setLastName(suser._lastName);
   user.setEmail(suser._email);
   user.setPassword(suser._password);
 
-  CouponModel.find({}, {}, {limit: perPageLimit, skip:(perPageLimit * pageNum)}, function(err, couponsFromDB) {
+  CouponModel.find({}, {}, { limit: perPageLimit, skip: (perPageLimit * pageNum) }, function (err, couponsFromDB) {
     if (err) throw err;
-    rtrieveAndDisplay(couponsFromDB,coupons,res, user, perPageLimit);
-    next();
+    CouponModel.count({}, function (err, count) {
+      pagesToDisp = count / perPageLimit;
+      rtrieveAndDisplay(couponsFromDB, coupons, res, user, pagesToDisp);
+      next();
+    });    
   });
 });
 
-couponRouter.get('/dashboard', function(req, res, next) {
+couponRouter.get('/dashboard', function (req, res, next) {
   var couponSession = req.session;
   var suser = couponSession.user;
   let user = new User();
@@ -68,27 +76,26 @@ couponRouter.get('/dashboard', function(req, res, next) {
   user.setEmail(suser._email);
   user.setPassword(suser._password);
 
-    res.render('userDashbord',{
-      user:user
-    });
+  res.render('userDashbord', {
+    user: user
+  });
 });
 
-function rtrieveAndDisplay(couponsFromDB, coupons, res, user, perPageLimit) {
-  couponsFromDB.forEach(function(element) {
-        let coupon = new Coupon();
+function rtrieveAndDisplay(couponsFromDB, coupons, res, user, pagesToDisp) {
+  couponsFromDB.forEach(function (element) {
+    let coupon = new Coupon();
 
-        coupon.setCouponName(element.coupon_name);
-        coupon.setCouponCategory(element.coupon_categoty);
-        coupon.setFileBinData(element.binary_data);  
-            
-        coupons.push(coupon);          
-  });  
+    coupon.setCouponName(element.coupon_name);
+    coupon.setCouponCategory(element.coupon_categoty);
+    coupon.setFileBinData(element.binary_data);
+
+    coupons.push(coupon);
+  });
   res.render('myCoupons', {
     user: user,
     coupons: coupons,
-    pagesToDisp:(coupons.length)/perPageLimit
-  });
-  log.info((coupons.length)/perPageLimit);
+    pagesToDisp: pagesToDisp
+  }); 
 }
 
 module.exports = couponRouter;
