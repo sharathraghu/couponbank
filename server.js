1d523d98771df19ab54e67c34d4e35307a714f18
Sharath Mg <sharathraghu@gmail.com>
2017-09-07 15:46:59 -0700

    validation changes

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ var app = express();
 
 app.use("/asset", express.static(__dirname + '/dist/'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validate());
 app.use(fileUploadUtil());
 app.use(session({ store: new redisStore({host: 'pub-redis-18816.us-west-2-1.1.ec2.garantiadata.com', port: 18816, client: redisClient, pass:'bankcoupon'}), secret: 'CouponBank', resave: false, saveUninitialized: false, cookie: { maxAge: 1800000 }, name: 'id' }));
 app.use(csurf());
-app.use(validate());
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 app.use(userRouter, couponRouter, commonRouter);
